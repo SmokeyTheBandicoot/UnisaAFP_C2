c68dfdc1de8f19e89e14ca0fcbcfa52b1f9137ac
Michele Gargiulo <m.gargiulo30@studenti.unisa.it>
2019-07-03 14:41:33 +0200

    first commit test

---
+++ Sample App/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample App
+//
+//  Created by Michele Gargiulo on 02/07/2019.
+//  Copyright © 2019 Michele Gargiulo. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
