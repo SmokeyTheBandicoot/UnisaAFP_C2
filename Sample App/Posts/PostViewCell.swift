fe90975b986e3c11111b39bcc2246ed7e366982c
Michele Gargiulo <m.gargiulo30@studenti.unisa.it>
2019-07-03 14:41:11 +0200

    First commit

---
+++ Sample App/Posts/PostViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostViewCell.swift
+//  Sample App
+//
+//  Created by Michele Gargiulo on 03/07/2019.
+//  Copyright © 2019 Michele Gargiulo. All rights reserved.
+//
+
+import UIKit
+
+class PostViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
