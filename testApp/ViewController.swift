443cf99bdd3636820a8a70af5eb29de6ae54a9b2
Michele Gargiulo <m.gargiulo30@studenti.unisa.it>
2019-07-04 16:48:58 +0200

    commit 3 + Added DataRecorder + Added DataRecord + Added logic to actually register data
    NOTES:
    o logic for data persistency is not yet in-place
    o GUI is still very pretty bare-bones

--- testApp/ViewController.swift
+++ testApp/ViewController.swift
@@ -11,6 +11,10 @@ import CoreMotion
 
 class ViewController: UIViewController {
 
+    // Record Manager Variables
+    var dataRecorder: DataRecorder = DataRecorder()
+    
+    // Sensor variables
     let motionManager = CMMotionManager()
     var timer: Timer!
     
@@ -34,16 +38,41 @@ class ViewController: UIViewController {
     }
     
     @objc func update() {
+        
+        var acc: [Coord: Double] = [Coord.x: 0.0, Coord.y: 0.0, Coord.z: 0.0]
+        var gyr: [Coord: Double] = [Coord.x: 0.0, Coord.y: 0.0, Coord.z: 0.0]
+        var face: [FaceIDAngle: Double] = [FaceIDAngle.roll: 0.0, FaceIDAngle.yaw: 0.0]
+        var app: String = "No App"
+        
         if let accelerometerData = motionManager.accelerometerData {
             txtx.text=String(format: "%.3f",accelerometerData.acceleration.x)
+            acc[Coord.x] = accelerometerData.acceleration.x
             txty.text=String(format: "%.3f",accelerometerData.acceleration.y)
+            acc[Coord.y] = accelerometerData.acceleration.y
             txtz.text=String(format: "%.3f",accelerometerData.acceleration.z)
+            acc[Coord.z] = accelerometerData.acceleration.z
             
         }
+        
         if let gyroData = motionManager.gyroData {
             txtx2.text=String(format: "%.3f",gyroData.rotationRate.x)
+            gyr[Coord.x] = gyroData.rotationRate.x
             txty2.text=String(format: "%.3f",gyroData.rotationRate.y)
+            gyr[Coord.y] = gyroData.rotationRate.y
             txtz2.text=String(format: "%.3f",gyroData.rotationRate.z)
+            gyr[Coord.z] = gyroData.rotationRate.z
         }
+        
+        dataRecorder.appendRecord(record:
+            DataRecord(
+            // Record Accel Data
+            AccelData: [Coord.x: acc[Coord.x]!, Coord.y: acc[Coord.y]!, Coord.z: acc[Coord.z]!],
+                // Record Gyroscope Data
+            GyrData: [Coord.x: gyr[Coord.x]!, Coord.y: gyr[Coord.y]!, Coord.z: gyr[Coord.z]!],
+            // Record Face ID Data
+            FaceIDData: [FaceIDAngle.roll: face[FaceIDAngle.roll]!, FaceIDAngle.yaw: face[FaceIDAngle.yaw]!],
+            // Record App Data
+            curApp: app))
+        
       }
 }
